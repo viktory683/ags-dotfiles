b03ae96469a3feefd1ad9357e2eb9fc3b6a0756b
bzglve <viktory683@gmail.com>
2024-05-08 16:24:42 +0600

    refactor: Improve readability and consistency in SysTray component
    This commit enhances the SysTray component by improving its readability and consistency. Changes include:
    
    - Encapsulating SystemTray items inside an EventBox component for each item
    - Updating event handlers to reference the current item directly
    - Adding appropriate class names and spacing to the container Box
    
    These changes improve code organization and maintainability, making the SysTray component more robust and easier to understand.

--- widget/statusbar/buttons/SysTray.ts
+++ widget/statusbar/buttons/SysTray.ts
@@ -4,18 +4,19 @@ import SystemTray from 'resource:///com/github/Aylur/ags/service/systemtray.js';
 import Widget from 'resource:///com/github/Aylur/ags/widget.js';
 
 export default () =>
-    Widget.Box().hook(SystemTray, (self) => {
+    Widget.Box({
+        class_names: ['tray'],
+        spacing: 4,
+    }).hook(SystemTray, (self) => {
         self.children = SystemTray.items
             .sort((a, b) => (a.title > b.title ? -1 : 1))
-            .map((item) =>
+            .map((i) =>
                 EventBox({
-                    on_primary_click: (_: any, e: Gdk.Event) =>
-                        item.activate(e),
-                    on_secondary_click: (_: any, e: Gdk.Event) =>
-                        item.openMenu(e),
-                    class_names: ['widget', 'tray'],
-                    children: [Widget.Icon().bind('icon', item, 'icon')],
-                    tooltipText: item.bind('tooltip_markup'),
+                    on_primary_click: (_: any, e: Gdk.Event) => i.activate(e),
+                    on_secondary_click: (_: any, e: Gdk.Event) => i.openMenu(e),
+                    class_names: ['widget', 'item'],
+                    children: [Widget.Icon().bind('icon', i, 'icon')],
+                    tooltipText: i.bind('tooltip_markup'),
                 }),
             );
     });
